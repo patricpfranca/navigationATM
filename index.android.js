f06003457be1392efe9501980767dc9b0497fbc2
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-10 01:14:40 -0200

    modificando o statubar

--- index.android.js
+++ index.android.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, StatusBar, View } from 'react-native';
 
 import BarraNavegacao from './src/components/barraNavegacao.android';
 
 export default class app5 extends Component {
   render() {
     return (
-      <BarraNavegacao />
+      <View>
+        <StatusBar backgroundColor = '#ccc' />
+        <BarraNavegacao />
+      </View>
     );
   }
 }
