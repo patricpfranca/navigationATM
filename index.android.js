7e6f0abd1a28e842c27519d21f38cd50c92ddf63
Patric Pereira <patric.pereirafranca@gmail.com>
2019-02-10 17:10:49 -0200

    ajustando ids das rotas e implementando botão voltar de contatos

--- index.android.js
+++ index.android.js
@@ -4,19 +4,24 @@ import { Navigator } from 'react-native-deprecated-custom-components';
 
 import CenaPrincipal from './src/components/cenaPrincipal.android';
 import CenaClientes from './src/components/cenaClientes.android';
+import CenaContatos from './src/components/cenaContatos.android';
 
 export default class app5 extends Component {
   render() {
     return (
       <Navigator
-        initialRoute={{ id: 'a' }}
+        initialRoute={{ id: 'principal' }}
         renderScene={(route, navigator) => {
-          if (route.id === 'a') {
+          if (route.id === 'principal') {
             return (<CenaPrincipal navigator={navigator} />);
           }
 
-          if (route.id === 'b') {
+          if (route.id === 'clientes') {
             return (<CenaClientes navigator={navigator} />);
+          }
+
+          if (route.id === 'contatos') {
+            return (<CenaContatos navigator={navigator} />);
           }
         }}
       />
